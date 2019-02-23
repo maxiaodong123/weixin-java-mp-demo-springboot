50ef573a51d13af1e645bb3b27d53d87ff9b17f8
maxiaodong <34213333@qq.com>
2019-02-23 16:40:15 +0800

    fix bug3

--- src/main/java/com/github/binarywang/demo/wx/mp/job/WechatJob.java
+++ src/main/java/com/github/binarywang/demo/wx/mp/job/WechatJob.java
@@ -15,24 +15,24 @@ public class WechatJob {
 //    String appid;
 //    @Value(value = "${wechat.secret}")
 //    String secret;
-
-//    @Scheduled(cron = "${job.wechat_access_token.cron}")
-////    @Scheduled(cron = "cron = 0 0/1 * * * ? ")
-//    public void wechatAccessToken(){
 //
-//        String appid = "wxa64b9ed20f0e49c5";
-//        String secret = "a78e2ef6d68dc285ec6d6bd295b82536";
-//        String access_token = "";//凭据
-//        int expires_in = -1;//凭据有效时间
-//        String ticket = "";
-//        int ticket_expires_in = -1;
-//        try {
-//            AccessToken accessToken = WeixinUtil.getAccessToken(appid, secret);
-//            access_token = accessToken.getToken();
-//            expires_in = accessToken.getExpiresIn();
-//            System.out.println("access_token为" + access_token + "，超时时间为：" + expires_in);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+//    @Scheduled(cron = "${job.wechat_access_token.cron}")
+//    @Scheduled(cron = "cron = 0 0/1 * * * ? ")
+    public void wechatAccessToken(){
+
+        String appid = "wxa64b9ed20f0e49c5";
+        String secret = "a78e2ef6d68dc285ec6d6bd295b82536";
+        String access_token = "";//凭据
+        int expires_in = -1;//凭据有效时间
+        String ticket = "";
+        int ticket_expires_in = -1;
+        try {
+            AccessToken accessToken = WeixinUtil.getAccessToken(appid, secret);
+            access_token = accessToken.getToken();
+            expires_in = accessToken.getExpiresIn();
+            System.out.println("access_token为" + access_token + "，超时时间为：" + expires_in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
